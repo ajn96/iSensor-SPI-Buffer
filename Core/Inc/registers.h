fca5e5b68c647fd5c338883603356f56a89c64af
anolan3 <Alex.Nolan@analog.com>
2020-03-19 15:24:25 -0400

    Added definition for some identifier registers

--- Core/Inc/registers.h
+++ Core/Inc/registers.h
@@ -45,6 +45,11 @@ void ProcessCommand();
 #define USER_SCR_1_REG			0x0A
 #define USER_SCR_2_REG			0x0B
 #define USER_SCR_3_REG			0x0C
+#define FW_MONTH_DAY			0x3B
+#define FW_YEAR					0x3C
+#define FW_REV					0x3D
+#define DEV_SN_LOW				0x3E
+#define DEV_SN_HIGH				0x3F
 
 /** iSensor-SPI-Buffer buffer Tx data page */
 #define BUF_WRITE_PAGE			254
