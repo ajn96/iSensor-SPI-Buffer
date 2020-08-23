e5224c2217d8256427edb1cb20275f38f5ed6547
anolan3 <Alex.Nolan@analog.com>
2020-08-23 00:18:51 -0400

    Implemented ADC sampling for temp and vdd. Vdd has an offset from the real vdd for some reason

--- Core/Src/adc.c
+++ Core/Src/adc.c
@@ -10,8 +10,7 @@
 
 #include "adc.h"
 
-static void ADC1Init();
-static void ADC1Start();
+static void ProcessTempReading();
 static int16_t ScaleTempData(uint32_t rawTemp);
 static uint16_t GetVdd(uint32_t VrefMeasurement);
 
@@ -21,17 +20,73 @@ static ADC_HandleTypeDef hadc1;
 /** Global register array. (from registers.c) */
 extern volatile uint16_t g_regs[];
 
+
 /**
-  * @brief Init temp sensor
+  * @brief ADC1 Initialization Function
   *
-  * @return void
+  * @return None
   *
-  * This function should be called once as part of the firmware initialization process
+  * This function should be called once as part of the firmware initialization process.
+  *
+  * Initializes ADC1 in single sampling mode, with two input
+  * channels converted (temp sensor and VREFINT). The sample time
+  * is set to 181 ADC cycles. The ADC is clocked from 72MHz core clock
+  * divided by 8. 181 cycles / (72MHz/8) -> 20.1us. The Temp sensor
+  * requires a minimum 10us setting time for accurate measurements.
   */
 void ADCInit()
 {
-	ADC1Init();
-	ADC1Start();
+	ADC_MultiModeTypeDef multimode = {0};
+	ADC_ChannelConfTypeDef sConfig = {0};
+
+	/* Common config */
+	hadc1.Instance = ADC1;
+	hadc1.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
+	hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+	hadc1.Init.ScanConvMode = ENABLE;
+	hadc1.Init.ContinuousConvMode = DISABLE;
+	hadc1.Init.DiscontinuousConvMode = ENABLE;
+	hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+	hadc1.Init.NbrOfConversion = 2;
+	hadc1.Init.DMAContinuousRequests = DISABLE;
+	hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+	hadc1.Init.LowPowerAutoWait = DISABLE;
+	hadc1.Init.Overrun = ADC_OVR_DATA_PRESERVED;
+	hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+	if (HAL_ADC_Init(&hadc1) != HAL_OK)
+	{
+		Error_Handler();
+	}
+
+	/* Configure the ADC multi-mode */
+	multimode.Mode = ADC_MODE_INDEPENDENT;
+	if (HAL_ADCEx_MultiModeConfigChannel(&hadc1, &multimode) != HAL_OK)
+	{
+		Error_Handler();
+	}
+
+	/* Configure the ADC temp sensor channel */
+	sConfig.Channel = ADC_CHANNEL_TEMPSENSOR;
+	sConfig.Rank = 1;
+	sConfig.SamplingTime = ADC_SAMPLETIME_181CYCLES_5;
+	sConfig.Offset = 0;
+	HAL_ADC_ConfigChannel(&hadc1, &sConfig);
+
+	/* Configure ADC vrefint channel */
+	sConfig.Channel = ADC_CHANNEL_VREFINT;
+	sConfig.Rank = 2;
+	sConfig.SamplingTime = ADC_SAMPLETIME_181CYCLES_5;
+	sConfig.Offset = 0;
+	HAL_ADC_ConfigChannel(&hadc1, &sConfig);
+
+	/* Run ADC self-cal */
+	if(HAL_ADCEx_Calibration_Start(&hadc1, ADC_SINGLE_ENDED) != HAL_OK)
+	{
+		Error_Handler();
+	}
+
+	/* Enable temp sensor */
+	ADC12_COMMON->CCR |= (1 << 23);
 }
 
 /**
@@ -46,6 +101,43 @@ void ADCInit()
   */
 void UpdateADC()
 {
+
+	static uint32_t adc_state = ADC_TEMP_START;
+
+	switch(adc_state)
+	{
+	case ADC_TEMP_START:
+		/* Start conversion, move to next state */
+		HAL_ADC_Start(&hadc1);
+		adc_state = ADC_TEMP_READ;
+		break;
+	case ADC_TEMP_READ:
+		if(!HAL_IS_BIT_CLR(hadc1.Instance->ISR, (ADC_FLAG_EOC | ADC_FLAG_EOS)))
+		{
+			ProcessTempReading();
+			adc_state = ADC_VDD_START;
+		}
+		break;
+	case ADC_VDD_START:
+		HAL_ADC_Start(&hadc1);
+		adc_state = ADC_VDD_READ;
+		break;
+	case ADC_VDD_READ:
+		if(!HAL_IS_BIT_CLR(hadc1.Instance->ISR, (ADC_FLAG_EOC | ADC_FLAG_EOS)))
+		{
+			/* Get VREFINT ADC value and scale to Vdd */
+			g_regs[VDD_REG] = GetVdd(hadc1.Instance->DR);
+			/* Back to temp */
+			adc_state = ADC_TEMP_START;
+		}
+		break;
+	default:
+		adc_state = ADC_TEMP_START;
+	}
+}
+
+static void ProcessTempReading()
+{
 	/* Read value from ADC and scale */
 	int16_t temp = ScaleTempData(hadc1.Instance->DR);
 
@@ -58,10 +150,6 @@ void UpdateADC()
 		g_regs[STATUS_0_REG] |= STATUS_TEMP_WARNING;
 		g_regs[STATUS_1_REG] = g_regs[STATUS_0_REG];
 	}
-
-	/* Get VREFINT ADC value and scale to Vdd */
-	temp = 0;
-	g_regs[VDD_REG] = GetVdd(temp);
 }
 
 /**
@@ -109,75 +197,4 @@ static int16_t ScaleTempData(uint32_t rawTemp)
 	result = result / divisor;
 	result += 300;
 	return result & 0xFFFF;
-}
-
-/**
-  * @brief ADC1 Initialization Function
-  *
-  * @return None
-  *
-  * Initializes ADC1 in continuous sampling mode. Only a single
-  * input is converted (temp sensor input channel). The sample time
-  * is set to 601 ADC cycles. The ADC is clocked from 72MHz core clock
-  * divided by 8. 601 cycles / (72MHz/8) -> 66us. The Temp sensor
-  * requires a minimum 10us setting time for accurate measurements.
-  */
-static void ADC1Init()
-{
-	ADC_MultiModeTypeDef multimode = {0};
-	ADC_ChannelConfTypeDef sConfig = {0};
-
-	/* Common config */
-	hadc1.Instance = ADC1;
-	hadc1.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
-	hadc1.Init.Resolution = ADC_RESOLUTION_12B;
-	hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
-	hadc1.Init.ContinuousConvMode = ENABLE;
-	hadc1.Init.DiscontinuousConvMode = DISABLE;
-	hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-	hadc1.Init.NbrOfConversion = 1;
-	hadc1.Init.DMAContinuousRequests = DISABLE;
-	hadc1.Init.EOCSelection = DISABLE;
-	hadc1.Init.LowPowerAutoWait = DISABLE;
-	hadc1.Init.Overrun = ADC_OVR_DATA_OVERWRITTEN;
-	if (HAL_ADC_Init(&hadc1) != HAL_OK)
-	{
-		Error_Handler();
-	}
-
-	/* Configure the ADC multi-mode */
-	multimode.Mode = ADC_MODE_INDEPENDENT;
-	if (HAL_ADCEx_MultiModeConfigChannel(&hadc1, &multimode) != HAL_OK)
-	{
-		Error_Handler();
-	}
-
-	/* Configure the ADC channel */
-	sConfig.Channel = ADC_CHANNEL_TEMPSENSOR;
-	sConfig.Rank = 1;
-	sConfig.SamplingTime = ADC_SAMPLETIME_601CYCLES_5;
-	sConfig.Offset = 0;
-	if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-	{
-		Error_Handler();
-	}
-
-	/* Run ADC self-cal */
-	if(HAL_ADCEx_Calibration_Start(&hadc1, ADC_SINGLE_ENDED) != HAL_OK)
-	{
-		Error_Handler();
-	}
-}
-
-/**
-  * @brief ADC1 Enable Function
-  *
-  * @return None
-  *
-  * Enables on-die temperature sensor and starts ADC1
-  */
-static void ADC1Start()
-{
-	ADC12_COMMON->CCR |= (1 << 23);
-	HAL_ADC_Start(&hadc1);
 }
