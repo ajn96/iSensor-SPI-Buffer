f8e10f126c57118f3f2f5766cf6d1dc7e3f93a3b
anolan3 <Alex.Nolan@analog.com>
2020-10-17 15:10:55 -0400

    Fleshed out example. Seems to be very performanced limited compared to something like tera-term. Will have to investigate more clever readline implementation

--- Utilities/example.py
+++ Utilities/example.py
@@ -1,4 +1,59 @@
 from spi_buf_cli import ISensorSPIBuffer
+import time
 
+#board conneted to COM11
 buf = ISensorSPIBuffer("COM11")
 print(buf.version())
+print(buf.check_connection())
+
+#select buf config page
+buf.select_page(253)
+
+#configure buffer settings for ADIS1649x
+buf.write_reg(0x2, 2) #IMU burst
+buf.write_reg(0x4, 38) # 38-byte burst read
+buf.write_reg(0x10, 0x805) # 2.25MHz spi clock, 5us stall
+
+#read regs back
+print("Config Page: " + str(buf.read_regs(0x0, 0x10)))
+
+#set burst trigger word
+buf.select_page(254)
+buf.write_reg(0x12, 0x7C00)
+
+#move to page 3 (ADIS1649x config page)
+buf.select_page(3)
+print("Decimate: " + str(buf.read_reg(0xC)))
+#set decimate for 400Hz data production
+buf.write_reg(0xC, 9)
+print("Decimate: " + str(buf.read_reg(0xC)))
+
+#set IMU to page 0
+buf.select_page(0)
+print("IMU Page 0: " + str(buf.read_regs(0x0, 0x26)))
+
+#start a stream (buffer captures data and automatically sends over USB)
+print("Start data count: " + str(buf.StreamData.qsize()))
+buf.start_stream()
+#get 2 sec of data
+time.sleep(2)
+buf.stop_stream()
+print("End data count: " + str(buf.StreamData.qsize()))
+
+bufEntry = buf.StreamData.get()
+timeStamp = (bufEntry[2] + (65536 * bufEntry[3])) / 1000000.0
+lastTimestamp = timeStamp
+startTime = timeStamp
+delta = 0.0
+maxDelta = 0.0
+while buf.StreamData.empty() == False:
+    bufEntry = buf.StreamData.get()
+    lastTimestamp = timeStamp
+    timeStamp = (bufEntry[2] + (65536 * bufEntry[3])) / 1000000.0
+    delta = timeStamp - lastTimestamp
+    if delta > maxDelta:
+        maxDelta = delta
+
+print("Starting buffer timestamp: " + str(startTime))
+print("Ending buffer timestamp: " + str(timeStamp))
+print("Max timestamp delta: " + str(maxDelta))
--- Utilities/spi_buf_cli.py
+++ Utilities/spi_buf_cli.py
@@ -22,26 +22,21 @@ class ISensorSPIBuffer():
 #API to the buffer board
 
     def select_page(self, page):
-        "Set the active iSensor-SPI-Buffer page. Pages less than 253 will select IMU"
-
-        #write page value to addr 0
+        "Set the active iSensor-SPI-Buffer page. Pages less than 253 will pass to IMU"
         self._SendLine("write 0 " + format(page, "x"))
 
     def read_regs(self, startAddr, endAddr):
         "Read multiple registers on current page, from startAddr to endAddr"
-        
         self._FlushSerialInput()
         self._SendLine("read " + format(startAddr, "x") + " " + format(endAddr, "x"))
         return self._ParseLine(self._ReadLine())
 
     def read_reg(self, addr):
         "Read a single register on current page, at address addr"
-
         return self.read_regs(addr, addr)[0]
 
-    def write_reg16(self, addr, value):
+    def write_reg(self, addr, value):
         "Write a full 16 bit value to a register, lower then upper"
-        
         lower = value & 0xFF
         upper = (value & 0xFF00) >> 8
         self._SendLine("write " + format(addr, "x") + " " + format(lower, "x"))
@@ -50,45 +45,56 @@ class ISensorSPIBuffer():
 
     def version(self):
         "Get iSensor-SPI-Buffer firmware version info"
-        
         self._FlushSerialInput()
         self._SendLine("about")
         return self._ReadLine().replace("\r\n", "")
 
     def status(self):
         "Get the iSensor-SPI-Buffer status register value"
-        
         self._FlushSerialInput()
         self._SendLine("status")
         return self._ParseLine(self._ReadLine())
 
     def run_command(self, cmdValue):
         "Execute iSensor-SPI-Buffer command"
-        
         self._SendLine("cmd " + format(cmdValue, "x"))
 
     def uptime(self):
-        #get iSensor-SPI-Buffer uptime (ms)
+        "Get iSensor-SPI-Buffer uptime (ms)"
         self._FlushSerialInput()
         self._SendLine("uptime")
         return int(self._ReadLine().replace("ms\r\n", ""))
 
     def check_connection(self):
         "Check CLI connection to the iSensor-SPI-Buffer"
-        
+        startPage = self.read_reg(0)
+        self.select_page(253)
+        origScrVal = self.read_reg(0x26)
+        newVal = self.uptime()
+        self.write_reg(0x26, newVal)
+        readBack = self.read_reg(0x26)
+        self.write_reg(0x26, origScrVal)
+        if self.read_reg(0x26) != origScrVal:
+            return False
+        self.select_page(startPage)
+        if(self.read_reg(0) != startPage):
+            return False
+        if readBack != newVal:
+            return False
+
+        #all checks pass
+        return True
+
     def start_stream(self):
         "Start stream. When a stream is running, data should be read from the StreamQueue"
-        
-        #start stream receive thread (starts streams)
         self.StreamThread.ThreadActive = True
         self.StreamThread.start()
-        
+
     def stop_stream(self):
         "Signal stream thread to stop"
-        
         self.StreamThread.ThreadActive = False
 
-#private helper functions (not actually private)
+#private helper functions (not actually private, but not recommended for end user use)
 
     def _SendLine(self, line):
         #transmit line to iSensor SPI buffer CLI
@@ -106,10 +112,13 @@ class ISensorSPIBuffer():
 
     def _ParseLine(self, line):
         #parse csv line data to numeric array
+        line = line.replace("\r", "")
+        line = line.replace("\n", "")
         splitLine = line.split(",")
         retVal = []
         for val in splitLine:
-            retVal.append(int(val, 16))
+            if val != '':
+                retVal.append(int(val, 16))
         return retVal
 
     def _Connect(self):
@@ -134,6 +143,9 @@ class StreamWork(Thread):
     def run(self):
         #read starting page
         self._StreamStartPage = self.buf.read_reg(0)
+        #flush firmware buffer contents prior to stream
+        self.buf.run_command(1)
+        time.sleep(0.1)
         #start stream on buffer board firmware
         self.buf._SendLine("stream 1")
         self.buf.select_page(255)
@@ -153,9 +165,6 @@ class StreamWork(Thread):
         time.sleep(0.1)
         #restore page
         self.buf.select_page(self._StreamStartPage)
-        #flush firmware buffer contents
-        self.buf.run_command(1)
-        time.sleep(0.1)
         #read page
         self.buf.read_reg(0)
         
